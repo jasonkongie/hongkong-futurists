db455ba7e6e57a6a28c0640bd78c9d30059622d0
jckongucsd <jckong@ucsd.edu>
2023-08-30 01:03:45 +0800

    questions in queue added. With some issues

--- src/components/CustomTerminal.tsx
+++ src/components/CustomTerminal.tsx
@@ -1,6 +1,6 @@
-import React, {useEffect, useMemo} from 'react';
-import {Terminal} from "./Terminal/Terminal.tsx";
-import {useTerminal} from "./Terminal/hooks.tsx";
+import React, { useEffect, useMemo, useState } from 'react';
+import { Terminal } from "./Terminal/Terminal.tsx";
+import { useTerminal } from "./Terminal/hooks.tsx";
 
 function CustomTerminal() {
   const {
@@ -10,46 +10,64 @@ function CustomTerminal() {
     resetTerminal,
   } = useTerminal();
 
+  // Questions to ask the user.
+  const questions = [
+    "On a scale of 1 to 10, how important is continuous learning in your profession?",
+    "How often, on a scale from 1 (never) to 10 (always), do you seek feedback on your work?",
+    "Rate your ability to work in a team environment on a scale of 1 (not at all) to 10 (extremely well).",
+    "How do you rate the importance of work-life balance in your career? (1 being not important, 10 being extremely important).",
+    "On a scale from 1 to 10, how comfortable are you with taking on leadership roles in projects?",
+    "What benefits do you envision yourself obtaining from this organization, and in what ways do you see yourself contributing to our mission and objectives?",
+    "Can you describe your short-term and long-term career planning? How do you see your role evolving in this industry?",
+    "In your opinion, what significant impact or change do you wish to bring to your industry? How do you plan to achieve this during your tenure with us?"
+  ];
+
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const isCurrentQuestionIntegerType = currentQuestionIndex < 5; // First 5 questions are integer type.
 
   useEffect(() => {
     resetTerminal();
 
     pushToHistory(<>
-        <div><strong>Welcome!</strong> to the terminal.</div>
-        <div style={{fontSize: 20}}>It contains <span style={{color: 'yellow'}}><strong>HTML</strong></span>. Awesome, right?</div>
-        <br/>
-        <div>You can write: start or alert , to execute some commands.</div>
-      </>
-    );
+      <div><strong>Welcome!</strong> to the Hong Kong Futurist's application terminal.</div>
+      <div style={{ fontSize: 20 }}>The <span style={{ color: 'yellow' }}><strong>most exclusive</strong></span> student business league in California</div>
+      <br />
+      <div>You can write: start or alert, or answer the questions as they appear.</div>
+    </>);
   }, []);
 
   const commands = useMemo(() => ({
     'start': async () => {
-      await pushToHistory(<>
-          <div>
-            <strong>Starting</strong> the server... <span style={{color: 'green'}}>Done</span>
-          </div>
-        </>);
+      await pushToHistory(<div><strong>Starting</strong> the server... <span style={{ color: 'green' }}>Done</span></div>);
     },
     'alert': async () => {
       alert('Hello!');
-      await pushToHistory(<>
-          <div>
-            <strong>Alert</strong>
-            <span style={{color: 'orange', marginLeft: 10}}>
-              <strong>Shown in the browser</strong>
-            </span>
-          </div>
-        </>);
+      await pushToHistory(<div><strong>Alert</strong><span style={{ color: 'orange', marginLeft: 10 }}><strong>Shown in the browser</strong></span></div>);
     },
-  }), [pushToHistory]);
+    'answer': async (input) => {
+      if (isCurrentQuestionIntegerType) {
+        const intValue = parseInt(input, 10);
+        if (intValue >= 1 && intValue <= 10) {
+          await pushToHistory(<div>Your answer {intValue} is saved!</div>);
+          setCurrentQuestionIndex(current => current + 1);
+        } else {
+          await pushToHistory(<div>Invalid input. Please provide an integer between 1 and 10.</div>);
+        }
+      } else {
+        // Handle short-answer questions here.
+        // For this example, just move to the next question.
+        await pushToHistory(<div>Your answer is saved!</div>);
+        setCurrentQuestionIndex(current => current + 1);
+      }
+    },
+  }), [pushToHistory, isCurrentQuestionIntegerType]);
 
   return (
     <div className="CustomTerminal">
       <Terminal
         history={history}
         ref={setTerminalRef}
-        promptLabel={<>Write something awesome:</>}
+        promptLabel={currentQuestionIndex < questions.length ? <>{questions[currentQuestionIndex]}</> : <>Write something awesome:</>}
         commands={commands}
       />
     </div>
