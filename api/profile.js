3688fe153a46065b334aea9098aa1a9d2b8e4dac
jckongucsd <jckong@ucsd.edu>
2023-11-02 22:00:03 -0700

    we good?

--- api/profile.js
+++ api/profile.js
@@ -5,17 +5,8 @@ module.exports = (req, res) => {
       res.status(200).send('Webhook received');
     } else {
       // Handle any other HTTP method
-      res.status(405).send('Method Not ?? Allowed');
+      res.status(405).send('Method Not Allowed');
     }
   };
-  
-  // /api/profile.js (temporary debugging purpose)
-// module.exports = (req, res) => {
-//     if (req.method === 'GET') {
-//       res.status(200).send('GET request to the profile page');
-//     } else {
-//       // Handle any other HTTP method
-//       res.status(405).send('Method Not Allowed');
-//     }
-//   };
+
   
