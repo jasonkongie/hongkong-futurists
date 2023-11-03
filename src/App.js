9843f345822b6e201ee71b34acf815ba53305fef
jckongucsd <jckong@ucsd.edu>
2023-11-03 14:38:26 -0700

    typeform-webhooks stuff, old

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from './components/HomePage';
 import CustomTerminal from './components/CustomTerminal.tsx';
 import RedirectComponent  from './components/Redirect/RedirectComponent';
 import ProfilePage from './components/ProfilePage';
+import SignIn from './components/SignIn';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/terminal" element={<CustomTerminal />} />
         <Route path="/apply" element={<RedirectComponent targetUrl="https://p4kq9oknrl1.typeform.com/to/LJUtAcbH" />} />
         <Route path="/profile/:profileId" element={<ProfilePage />} />
+        <Route path="/signIn" element={<SignIn />} />
       </Routes>
     </Router>
   );
